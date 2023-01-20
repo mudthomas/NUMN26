4fff340d5b61a0faf2bc4859a1ee435f8c44633c
mud <mudthomas@gmail.com>
2023-01-20 13:21:14 +0100

    Tests several values of k

--- Project1/project1.py
+++ Project1/project1.py
@@ -7,16 +7,18 @@ import matplotlib.pyplot as mpl
 import numpy as np
 
 
+
 def f(t, y):
-    k = 1
+    # k=1
     lam = k * (np.sqrt(y[0]**2+y[1]**2) - 1)/np.sqrt(y[0]**2+y[1]**2)
     return np.asarray([y[2], y[3], -y[0]*lam, -y[1]*lam -1])
 
 starting_point = np.array([1,1,1,1])
-
 problem = Explicit_Problem(f, y0=starting_point)
-problem.name = "Task 1"
 
-problem_cvode = CVode(problem)
-problem_cvode.simulate(10.0, 100)
-problem_cvode.plot()
+
+for k in [0, 0.1, 0.5, 1, 5, 10, 50, 100, 500, 1000]:
+    problem.name=f"Task 1, k={k}"
+    problem_cvode = CVode(problem)
+    problem_cvode.simulate(10.0, 100)
+    problem_cvode.plot()
