f6f37e0733153a223fa6b89cbf12670d7c781e4e
mud <mudthomas@gmail.com>
2023-01-20 18:11:51 +0100

    Add BDF3 and BDF4

--- Project1/project1.py
+++ Project1/project1.py
@@ -1,5 +1,7 @@
 from assimulo.solvers.sundials import CVode
 from assimulo.ode import Explicit_Problem
+from assimulo.explicit_ode import Explicit_ODE
+
 import numpy as np
 
 
@@ -8,12 +10,138 @@ def f(t, y):
     lam = k * (np.sqrt(y[0]**2+y[1]**2) - 1)/np.sqrt(y[0]**2+y[1]**2)
     return np.asarray([y[2], y[3], -y[0]*lam, -y[1]*lam - 1])
 
-
-starting_point = np.array([1, 1, 1, 1])
+starting_point = np.array([1, 0, 0, -1])
 problem = Explicit_Problem(f, y0=starting_point)
 
 for k in [0, 0.1, 0.5, 1, 5, 10, 50, 100, 500, 1000]:
     problem.name = f"Task 1, k={k}"
-    problem_cvode = CVode(problem)
-    problem_cvode.simulate(10.0, 100)
-    problem_cvode.plot()
+    solver = CVode(problem)
+    solver.simulate(10.0, 100)
+    solver.plot()
+
+
+class BDF_3(Explicit_ODE):
+    def __init__(self, problem):
+        Explicit_ODE.__init__(self, problem)
+        self.maxsteps = 500
+        self.maxit = 100
+        self.tol = 1.e-8
+
+    def integrate(self, t0, tf, y0):
+        t_list = [t0]
+        y_list = [y0]
+        # one step EE
+        t, y = self.EEstep(t0, y0, h)
+        t_list.append(t)
+        y_list.append(y)
+        # one step BDF2
+        t, y = self.BDF2step(t, y_list[-1], y_list[-2], h)
+        t_list.append(t)
+        y_list.append(y)
+        for i in range(2, self.maxsteps):
+            if t >= tf:
+                break
+            # BDF3
+            t, y = self.BDF3step(t, y_list[-1], y_list[-2], y_list[-3], h)
+            t_list.append(t)
+            y_list.append(y)
+        pass
+
+    def EEstep(self, t_n, y_n, h):
+        t_np1 = t_n + h
+        y_np1 = y_n + h*self.problem.rhs(t_n, y_n)
+        return t_np1, y_np1
+
+    def BDF2step(self, t_n, y_n, y_nm1, h):
+        t_np1 = t_n + h
+        y_np1 = y_n
+        for i in range(self.maxit):
+            y_np1_old = y_np1
+            y_np1 = (4*y_n - 1*y_nm1 + 2*h*self.problem.rhs(t_np1, y_np1))/3
+
+            if(np.norm(y_np1-y_np1_old)) < self.tol:
+                return t_np1, y_np1
+        else:
+            raise Explicit_ODE_Exception
+
+    def BDF3step(self, t_n, y_n, y_nm1, y_nm2):
+        t_np1 = t_n + h
+        y_np1 = y_n
+        for i in range(self.maxit):
+            y_np1_old = y_np1
+            y_np1 = (18*y_n - 9*y_nm1 + 2*y_nm2 + 6*h*self.problem.rhs(t_np1, y_n))/11
+            if(np.norm(y_np1-y_np1_old)) < self.tol:
+                return t_np1, y_np1
+        else:
+            raise Explicit_ODE_Exception
+
+
+class BDF_4(Explicit_ODE):
+    def __init__(self, problem):
+        Explicit_ODE.__init__(self, problem)
+        self.maxsteps = 500
+        self.maxit = 100
+        self.tol = 1.e-8
+
+    def integrate(self, t0, tf, y0):
+        t_list = [t0]
+        y_list = [y0]
+        # one step EE
+        t, y = self.EEstep(t0, y0, h)
+        t_list.append(t)
+        y_list.append(y)
+        # one step BDF2
+        t, y = self.BDF2step(t, y_list[-1], y_list[-2], h)
+        t_list.append(t)
+        y_list.append(y)
+        # one step BDF3
+        t, y = self.BDF2step(t, y_list[-1], y_list[-2], y_list[-3], h)
+        t_list.append(t)
+        y_list.append(y)
+        for i in range(3, self.maxsteps):
+            if t >= tf:
+                break
+            # BDF4
+            t, y = self.BDF4step(t, y_list[-1], y_list[-2], y_list[-3], y_list[-4], h)
+            t_list.append(t)
+            y_list.append(y)
+        pass
+
+    def EEstep(self, t_n, y_n, h):
+        t_np1 = t_n + h
+        y_np1 = y_n + h*self.problem.rhs(t_n, y_n)
+        return t_np1, y_np1
+
+    def BDF2step(self, t_n, y_n, y_nm1, h):
+        t_np1 = t_n + h
+        y_np1 = y_n
+        for i in range(self.maxit):
+            y_np1_old = y_np1
+            y_np1 = (4*y_n - 1*y_nm1 + 2*h*self.problem.rhs(t_np1, y_np1))/3
+
+            if(np.norm(y_np1-y_np1_old)) < self.tol:
+                return t_np1, y_np1
+        else:
+            raise Explicit_ODE_Exception
+
+    def BDF3step(self, t_n, y_n, y_nm1, y_nm2):
+        t_np1 = t_n + h
+        y_np1 = y_n
+        for i in range(self.maxit):
+            y_np1_old = y_np1
+            y_np1 = (18*y_n - 9*y_nm1 + 2*y_nm2 + 6*h*self.problem.rhs(t_np1, y_np1))/11
+            if(np.norm(y_np1-y_np1_old)) < self.tol:
+                return t_np1, y_np1
+        else:
+            raise Explicit_ODE_Exception
+
+    def BDF4step(self, t_n, y_n, y_nm1, y_nm2, y_nm3, h):
+        t_np1 = t_n + h
+        y_np1 = y_n
+        for i in range(self.maxit):
+            y_np1_old = y_np1
+            y_np1 = (48*y_n - 36*y_nm1 + 16*y_nm2 - 3*y_nm3 + 12*h*self.problem.rhs(t_np1, y_np1))/25
+            if(np.norm(y_np1-y_np1_old)) < self.tol:
+                return t_np1, y_np1
+        else:
+            raise Explicit_ODE_Exceptions
