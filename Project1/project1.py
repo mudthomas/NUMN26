d7474bc704fd5f83e80ac5e20826822283b3f9ce
mud <mudthomas@gmail.com>
2023-01-20 12:29:28 +0100

    Actually did Task1

--- Project1/project1.py
+++ Project1/project1.py
@@ -1,20 +1,21 @@
 from assimulo.explicit_ode import Explicit_ODE
+from assimulo.solvers.sundials import CVode
 from assimulo.ode import *
 import numpy as np
 import matplotlib.pyplot as mpl
 
 import numpy as np
-from BDF2_Assimulo import BDF_2
 
-def f(t, y):
-    k = 10
+
+def f(t, y, k):
     lam = k * (np.sqrt(y[0]**2+y[1]**2) - 1)/np.sqrt(y[0]**2+y[1]**2)
     return np.asarray([y[2], y[3], -y[0]*lam, -y[1]*lam -1])
 
-pend_mod=Explicit_Problem(f, y0=np.array([1,1,1,1]))
-pend_mod.name="Task 1"
+initial_guess = np.array([1,1,1,1])
 
-exp_sim = BDF_2(pend_mod) #Create a BDF solver
-t, y = exp_sim.simulate(1)
-exp_sim.plot()
-mpl.show()
+problem = Explicit_Problem(f, y0=initial_guess, p0=10)
+problem.name="Task 1"
+
+problem_cvode = CVode(problem)
+problem_cvode.simulate(10.0, 100)
+problem_cvode.plot()
