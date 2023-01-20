4b1579ba0c6a57e38228f830dd80c35a1f4821e8
mud <mudthomas@gmail.com>
2023-01-20 13:07:16 +0100

    Changed k to no longer be sensitivity-tested parameter

--- Project1/project1.py
+++ Project1/project1.py
@@ -7,14 +7,15 @@ import matplotlib.pyplot as mpl
 import numpy as np
 
 
-def f(t, y, k):
+def f(t, y):
+    k = 1
     lam = k * (np.sqrt(y[0]**2+y[1]**2) - 1)/np.sqrt(y[0]**2+y[1]**2)
     return np.asarray([y[2], y[3], -y[0]*lam, -y[1]*lam -1])
 
-initial_guess = np.array([1,1,1,1])
+starting_point = np.array([1,1,1,1])
 
-problem = Explicit_Problem(f, y0=initial_guess, p0=10)
-problem.name="Task 1"
+problem = Explicit_Problem(f, y0=starting_point)
+problem.name = "Task 1"
 
 problem_cvode = CVode(problem)
 problem_cvode.simulate(10.0, 100)
