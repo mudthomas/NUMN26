981d8b989d31be8245fb29ce1b2c641cad0b2189
mud <mudthomas@gmail.com>
2023-01-25 18:17:29 +0100

    Removed Claus BDF2-class

--- Project1/project1.py
+++ Project1/project1.py
@@ -120,110 +120,6 @@ class BDF_4(BDF_general):
     def __init__(self, problem):
         BDF_general.__init__(self, problem, 4)
 
-class BDF_2_Claus(Explicit_ODE):
-    """
-    BDF-2   (Example of how to set-up own integrators for Assimulo)
-    """
-    tol=1.e-8
-    maxit=100
-    maxsteps=500
-
-    def __init__(self, problem):
-        Explicit_ODE.__init__(self, problem) #Calls the base class
-
-        #Solver options
-        self.options["h"] = 0.01
-
-        #Statistics
-        self.statistics["nsteps"] = 0
-        self.statistics["nfcns"] = 0
-
-    def _set_h(self,h):
-            self.options["h"] = float(h)
-
-    def _get_h(self):
-        return self.options["h"]
-
-    h=property(_get_h,_set_h)
-
-    def integrate(self, t, y, tf, opts):
-        """
-        _integrates (t,y) values until t > tf
-        """
-        h = self.options["h"]
-        h = min(h, abs(tf-t))
-
-        #Lists for storing the result
-        tres = []
-        yres = []
-
-        for i in range(self.maxsteps):
-            if t >= tf:
-                break
-            self.statistics["nsteps"] += 1
-
-            if i==0:  # initial step
-                t_np1,y_np1 = self.step_EE(t,y, h)
-            else:
-                t_np1, y_np1 = self.step_BDF2([t,t_nm1], [y,y_nm1], h)
-            t,t_nm1=t_np1,t
-            y,y_nm1=y_np1,y
-
-            tres.append(t)
-            yres.append(y.copy())
-
-            h=min(self.h,np.abs(tf-t))
-        else:
-            raise Explicit_ODE_Exception('Final time not reached within maximum number of steps')
-
-        return ID_PY_OK, tres, yres
-
-    def step_EE(self, t, y, h):
-        """
-        This calculates the next step in the integration with explicit Euler.
-        """
-        self.statistics["nfcns"] += 1
-
-        f = self.problem.rhs
-        return t + h, y + h*f(t, y)
-
-    def step_BDF2(self,T,Y, h):
-        """
-        BDF-2 with Fixed Point Iteration and Zero order predictor
-
-        alpha_0*y_np1+alpha_1*y_n+alpha_2*y_nm1=h f(t_np1,y_np1)
-        alpha=[3/2,-2,1/2]
-        """
-        alpha=[3./2.,-2.,1./2]
-        f=self.problem.rhs
-
-        t_n,t_nm1=T
-        y_n,y_nm1=Y
-        # predictor
-        t_np1=t_n+h
-        y_np1_i=y_n   # zero order predictor
-        # corrector with fixed point iteration
-        for i in range(self.maxit):
-            self.statistics["nfcns"] += 1
-
-            y_np1_ip1=(-(alpha[1]*y_n+alpha[2]*y_nm1)+h*f(t_np1,y_np1_i))/alpha[0]
-            if SL.norm(y_np1_ip1-y_np1_i) < self.tol:
-                return t_np1,y_np1_ip1
-            y_np1_i=y_np1_ip1
-        else:
-            raise Explicit_ODE_Exception('Corrector could not converge within % iterations'%i)
-
-    def print_statistics(self, verbose=NORMAL):
-        self.log_message('Final Run Statistics            : {name} \n'.format(name=self.problem.name),        verbose)
-        self.log_message(' Step-length                    : {stepsize} '.format(stepsize=self.options["h"]), verbose)
-        self.log_message(' Number of Steps                : '+str(self.statistics["nsteps"]),          verbose)
-        self.log_message(' Number of Function Evaluations : '+str(self.statistics["nfcns"]),         verbose)
-
-        self.log_message('\nSolver options:\n',                                    verbose)
-        self.log_message(' Solver            : BDF2',                     verbose)
-        self.log_message(' Solver type       : Fixed step\n',                      verbose)
-
-
 
 for k in [1000]:
     problem_func = lambda t,y: f(t,y,k)
@@ -235,7 +131,7 @@ for k in [1000]:
     solver.simulate(1)
     solver.plot()
 
-    solver = BDF_2_Claus(problem)
+    solver = BDF_2(problem)
     solver.simulate(1)
     solver.plot()
 
