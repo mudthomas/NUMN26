34d4557e2434494498aa4fab4c69558e27beec93
mud <mudthomas@gmail.com>
2023-01-25 16:01:42 +0100

    Improved code

--- Project1/project1.py
+++ Project1/project1.py
@@ -22,40 +22,56 @@ def f(t, y):
 #     solver.plot()
 
 
-class BDF_3(Explicit_ODE):
-    def __init__(self, problem):
+class BDF_general(Explicit_ODE):
+    def __init__(self, problem, degree):
         Explicit_ODE.__init__(self, problem)
         self.maxsteps = 500
         self.maxit = 100
         self.tol = 1.e-8
+        self.degree = degree
+
+        #Solver options
+        self.options["h"] = 0.01
+        #Statistics
+        self.statistics["nsteps"] = 0
+        self.statistics["nfcns"] = 0
+
+    def _set_h(self,h):
+            self.options["h"] = float(h)
+
+    def _get_h(self):
+        return self.options["h"]
 
     def integrate(self, t, y, tf, opts):
-        t0 = t
-        y0 = y
-        h = 0.01
-        t_list = [t0]
-        y_list = [y0]
+        h = self.options["h"]
+        h = min(h, tf-t)  # Claus uses abs(tf-t), I'd rather have a check for tf>t.
+
+        t_list = [t]
+        y_list = [y]
         # one step EE
-        t, y = self.EEstep(t0, y0, h)
+        self.statistics["nsteps"] += 1
+        t, y = self.EEstep(t, y, h)
         t_list.append(t)
         y_list.append(y)
-        # one step BDF2
-        t, y = self.BDFstep_general(t, [y_list[-1], y_list[-2]], h)
-        t_list.append(t)
-        y_list.append(y)
-        for i in range(2, self.maxsteps):
+
+        for i in range(1, self.maxsteps):
             if t >= tf:
                 break
-            # BDF3
-            t, y = self.BDFstep_general(t, [y_list[-1], y_list[-2], y_list[-3]], h)
+            # BDF
+            self.statistics["nsteps"] += 1
+            try:
+                t, y = self.BDFstep_general(t, y_list[-self.degree:][::-1], h)
+            except:
+                t, y = self.BDFstep_general(t, y_list[::-1], h)
             t_list.append(t)
             y_list.append(y)
+            h = min(h, np.abs(tf-t))
+
         return ID_PY_OK, t_list, y_list
 
     def EEstep(self, t_n, y_n, h):
-        t_np1 = t_n + h
-        y_np1 = y_n + h*self.problem.rhs(t_n, y_n)
-        return t_np1, y_np1
+        self.statistics["nfcns"] += 1
+        return t_n + h, h*self.problem.rhs(t_n, y_n)
 
     def BDFstep_general(self, t_n, Y, h):
         coeffs=[[1, -1, -1],
@@ -69,92 +85,34 @@ class BDF_3(Explicit_ODE):
     def BDFstep_general_internal(self, t_n, Y, coeffs, h):
         t_np1 = t_n + h
         y_np1 = Y[0]
+
+        clump = 0
+        for i in range(len(Y)):
+            clump += coeffs[i+1] * Y[i]
+
         for i in range(self.maxit):
+            self.statistics["nfcns"] += 1
+
             y_np1_old = y_np1
-            clump = 0
-            for i in range(len(Y)):
-                clump += coeffs[i+1] * Y[i]
+            # FPI
+            # y_np1 = -(clump + coeffs[-1]*h*self.problem.rhs(t_np1, y_np1_old))/coeffs[0]
 
             # With fsolve
-            # temp_func = lambda y: coeffs[0]*y + (clump + coeffs[-1]*h*self.problem.rhs(t_np1, y))
-            # y_np1 = fsolve(temp_func, y_np1_old)
-
-            # FPI
-            y_np1 = -(clump + coeffs[-1]*h*self.problem.rhs(t_np1, y_np1))/coeffs[0]
+            temp_func = lambda y: coeffs[0]*y + (clump + coeffs[-1]*h*self.problem.rhs(t_np1, y))
+            y_np1 = fsolve(temp_func, y_np1_old)
 
             if(np.linalg.norm(y_np1-y_np1_old)) < self.tol:
                 return t_np1, y_np1
         else:
             raise Explicit_ODE_Exception(f"Corrector could not converge within {i} iterations")
 
-
-class BDF_4(Explicit_ODE):
+class BDF_3(BDF_general):
     def __init__(self, problem):
-        Explicit_ODE.__init__(self, problem)
-        self.maxsteps = 500
-        self.maxit = 100
-        self.tol = 1.e-8
+        BDF_general.__init__(self, problem, 3)
 
-    def integrate(self, t, y, tf, opts):
-        t0 = t
-        y0 = y
-        h = 0.01
-        t_list = [t0]
-        y_list = [y0]
-        # one step EE
-        t, y = self.EEstep(t0, y0, h)
-        t_list.append(t)
-        y_list.append(y)
-        # one step BDF2
-        t, y = self.BDF2step(t, [y_list[-1], y_list[-2]], h)
-        t_list.append(t)
-        y_list.append(y)
-        # one step BDF3
-        t, y = self.BDF3step(t, [y_list[-1], y_list[-2], y_list[-3]], h)
-        t_list.append(t)
-        y_list.append(y)
-        for i in range(3, self.maxsteps):
-            if t >= tf:
-                break
-            t, y = self.BDF4step(t, [y_list[-1], y_list[-2], y_list[-3], y_list[-4]], h)
-            t_list.append(t)
-            y_list.append(y)
-        return ID_PY_OK, t_list, y_list
-
-    def EEstep(self, t_n, y_n, h):
-        t_np1 = t_n + h
-        y_np1 = y_n + h*self.problem.rhs(t_n, y_n)
-        return t_np1, y_np1
-
-    def BDF2step(self, t_n, Y, h):
-        return self.BDFstep_general(t_n, Y, [3, -4, 1, -2], h)
-
-    def BDF3step(self, t_n, Y, h):
-        return self.BDFstep_general(t_n, Y, [11, -18, 9, -2, -6], h)
-
-    def BDF4step(self, t_n, Y, h):
-        return self.BDFstep_general(t_n, Y, [25, -48, 36, -16, 3, -12], h)
-
-    def BDFstep_general(self, t_n, Y, coeffs, h):
-        t_np1 = t_n + h
-        y_np1 = Y[0]
-        for i in range(self.maxit):
-            y_np1_old = y_np1
-            clump = 0
-            for i in range(len(Y)):
-                clump += coeffs[i+1] * Y[i]
-
-            # With fsolve
-            # temp_func = lambda y: coeffs[0]*y + (clump + coeffs[-1]*h*self.problem.rhs(t_np1, y))
-            # y_np1 = fsolve(temp_func, y_np1_old)
-
-            # FPI
-            y_np1 = -(clump + coeffs[-1]*h*self.problem.rhs(t_np1, y_np1))/coeffs[0]
-
-            if(np.linalg.norm(y_np1-y_np1_old)) < self.tol:
-                return t_np1, y_np1
-        else:
-            raise Explicit_ODE_Exception(f"Corrector could not converge within {i} iterations")
+class BDF_4(BDF_general):
+    def __init__(self, problem):
+        BDF_general.__init__(self, problem, 4)
 
 starting_point = np.array([1, 0, 0, -1])
 problem = Explicit_Problem(f, y0=starting_point)
