b580b1dc8f39e119bcc72975ecee5a825af2312c
mud <mudthomas@gmail.com>
2023-02-03 14:27:58 +0100

    Fix spelling error

--- Project1/BDF_FPI.py
+++ Project1/BDF_FPI.py
@@ -43,7 +43,7 @@ class BDF_general(Explicit_ODE):
 
     def integrate(self, t0, y0, tf, opts):
         """Solves the ODE problem.
-        Firs it takes one step with the explicit Euler method,
+        First it takes one step with the explicit Euler method,
         then it takes steps using the BDF method of increasing order util self.order is reached.
         After self.order number of steps it continues taking steps of that BDF method util reaching tf.
 
