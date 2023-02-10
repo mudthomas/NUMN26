50acc99b91b41c14c8bf2ac9abf31146f09a1059
mud <mudthomas@gmail.com>
2023-02-10 15:06:40 +0100

    Fix Gqq

--- Project2/squeezer.py
+++ Project2/squeezer.py
@@ -16,8 +16,6 @@ class Seven_bar_mechanism_general(Implicit_Problem):
 		self.y0, self.yd0 = self.init_squeezer()
 		self.t0 = 0
 		self.algvar = np.ones(len(self.y0))
-		self.rtol = 1.e-6
-		self.atol = 1.e-6 * np.ones(len(self.y0))
 
 	def reset(self):
 		self.y0, self.yd0 = self.init_squeezer()
@@ -41,7 +39,75 @@ class Seven_bar_mechanism_general(Implicit_Problem):
 			14222.4439199541138705911625887,        # betadotdot
 			-10666.8329399655854029433719415,       # Thetadotdot
 			0., 0., 0., 0., 0.]), np.zeros((6,))))
+
+		# new = opt.fsolve(self.Newton_init, 1e-6*np.ones(13,), args=y)
+		# yp = np.hstack((new[:7], np.array([
+		# 	14222.4439199541138705911625887,        # betadotdot
+		# 	-10666.8329399655854029433719415,       # Thetadotdot
+		# 	0., 0., 0., 0., 0.]), new[7:]))
 		return y, yp
+
+	def Newton_init(self, x, y):
+		# Inertia data
+		m1, m2, m3, m4, m5, m6, m7 = .04325, .00365, .02373, .00706, .07050, .00706, .05498
+		i1, i2, i3, i4, i5, i6, i7 = 2.194e-6, 4.410e-7, 5.255e-6, 5.667e-7, 1.169e-5, 5.667e-7, 1.912e-5
+
+		d, da, e, ea = 28.e-3, 115.e-4, 2.e-2, 1421.e-5
+		rr, ra = 7.e-3, 92.e-5
+		ss, sa, sb = 35.e-3, 1874.e-5, 1043.e-5
+		ta, tb = 2308.e-5, 916.e-5
+		u, ua, ub = 4.e-2, 1228.e-5, 449.e-5
+		zf, zt = 2.e-2, 4.e-2
+		fa = 1421.e-5
+
+		# Initial computations and assignments
+		beta, theta, gamma, phi, delta, omega, epsilon = y[0:7]
+		bep, thp, gap, php, dep, omp, epp = y[7:14]
+		sibe, sith, siga, siph, side, siom, siep = np.sin(y[0:7])
+		cobe, coth, coga, coph, code, coom, coep = np.cos(y[0:7])
+		sibeth, cobeth = np.sin(beta + theta), np.cos(beta + theta)
+		siphde, cophde = np.sin(phi + delta), np.cos(phi + delta)
+		siomep, coomep = np.sin(omega + epsilon), np.cos(omega + epsilon)
+
+		m = np.zeros((7, 7))
+		m[0, 0] = m1 * ra**2 + m2 * (rr**2 - 2 * da * rr * coth + da**2) + i1 + i2
+		m[1, 0] = m[0, 1] = m2 * (da**2 - da * rr * coth) + i2
+		m[1, 1] = m2 * da**2 + i2
+		m[2, 2] = m3 * (sa**2 + sb**2) + i3
+		m[3, 3] = m4 * (e - ea)**2 + i4
+		m[4, 3] = m[3, 4] = m4 * ((e - ea)**2 + zt * (e - ea) * siph) + i4
+		m[4, 4] = m4 * (zt**2 + 2 * zt * (e - ea) * siph + (e - ea)**2) + m5 * (ta**2 + tb**2) + i4 + i5
+		m[5, 5] = m6 * (zf - fa)**2 + i6
+		m[6, 5] = m[5, 6] = m6 * ((zf - fa)**2 - u * (zf - fa) * siom) + i6
+		m[6, 6] = m6 * ((zf - fa)**2 - 2 * u * (zf - fa) * siom + u**2) + m7 * (ua**2 + ub**2) + i6 + i7
+
+		gp = np.zeros((6, 7))
+		gp[0, 0] = - rr * sibe + d * sibeth
+		gp[0, 1] = d * sibeth
+		gp[0, 2] = - ss * coga
+		gp[1, 0] = rr * cobe - d * cobeth
+		gp[1, 1] = - d * cobeth
+		gp[1, 2] = - ss * siga
+		gp[2, 0] = - rr * sibe + d * sibeth
+		gp[2, 1] = d * sibeth
+		gp[2, 3] = - e * cophde
+		gp[2, 4] = - e * cophde + zt * side
+		gp[3, 0] = rr * cobe - d * cobeth
+		gp[3, 1] = - d * cobeth
+		gp[3, 3] = - e * siphde
+		gp[3, 4] = - e * siphde - zt * code
+		gp[4, 0] = - rr * sibe + d * sibeth
+		gp[4, 1] = d * sibeth
+		gp[4, 5] = zf * siomep
+		gp[4, 6] = zf * siomep - u * coep
+		gp[5, 0] = rr * cobe - d * cobeth
+		gp[5, 1] = - d * cobeth
+		gp[5, 5] = - zf * coomep
+		gp[5, 6] = - zf * coomep - u * siep
+
+		return np.vstack((np.hstack((m, gp.T)), np.hstack((gp, np.zeros((6, 6)))))) @ x
+
+
 
 	def res(self, t, y, yp):
 		"""
@@ -157,7 +223,7 @@ class Seven_bar_mechanism_general(Implicit_Problem):
 				- rr * cobe * v1**2 + d * cobeth * (v1 + v2)**2 + ss * siga * v3**2,
 				- rr * sibe * v1**2 + d * sibeth * (v1 + v2)**2 - ss * coga * v3**2,
 				- rr * cobe * v1**2 + d * cobeth * (v1 + v2)**2 + e * siphde * (v4 + v5)**2  + zt * code * v5**2,
-				- rr * sibe * v1 * 2 + d * sibeth * (v1 + v2)**2 - e * cophde * (v4 + v5)**2 + zt * side * v5**2,
+				- rr * sibe * v1 ** 2 + d * sibeth * (v1 + v2)**2 - e * cophde * (v4 + v5)**2 + zt * side * v5**2,
 				- rr * cobe * v1**2 + d * cobeth * (v1 + v2)**2 + zf * coomep * (v6 + v7)**2 + u * siep * v7**2,
 				- rr * sibe * v1**2 + d * sibeth * (v1 + v2)**2 + zf * siomep * (v6 + v7)**2 - u * coep * v7**2])
 			res_3 += np.dot(gp, yp[7:14])
@@ -171,9 +237,6 @@ class Seven_bar_mechanism_index3(Seven_bar_mechanism_general):
 		self.algvar[7:] = np.zeros(13)
 		self.index = 3
 
-		self.rtol = 1.e-6
-		self.atol = 1.e-6 * np.ones(len(self.y0))
-
 
 class Seven_bar_mechanism_index2(Seven_bar_mechanism_general):
 	def __init__(self):
@@ -181,30 +244,49 @@ class Seven_bar_mechanism_index2(Seven_bar_mechanism_general):
 		self.algvar[14:] = np.zeros(6)
 		self.index = 2
 
-		self.rtol = 1.e-6
-		self.atol = 1.e-6 * np.ones(len(self.y0))
-
 
 class Seven_bar_mechanism_index1(Seven_bar_mechanism_general):
 	def __init__(self):
 		Seven_bar_mechanism_general.__init__(self)
 		self.index = 1
 
-		self.rtol = 1.e-6
-		self.atol = 1.e-6 * np.ones(len(self.y0))
-
 
 if __name__ == "__main__":
 	import matplotlib.pyplot as plt
-	problem = Seven_bar_mechanism_index2()
 
-	# solver = IDA(problem)
-	solver = Radau5DAE(problem)
+	def doIndex3():
+		problem = Seven_bar_mechanism_index3()
+		solver = IDA(problem)
+		# solver = Radau5DAE(problem)
+		tol3 = 1.e-6 * np.ones(20)
+		tol3[7:] = 1.e5 * np.ones(13)
+		solver.atol = tol3
+		t, y, _ = solver.simulate(0.03)
+		plt.plot(t, np.fmod(y[:, 0:7], 2 * np.pi))
+		plt.ylim(-1, 1)
+		plt.show()
 
-	t, y, _ = solver.simulate(0.02)
-	plt.plot(t, np.fmod(y[:, 0:7], 2 * np.pi))
-	plt.ylim(-1, 1)
-	# solver.plot()
+	def doIndex2():
+		problem = Seven_bar_mechanism_index2()
+		solver = IDA(problem)
+		# solver = Radau5DAE(problem)
+		tol2 = 1.e-6 * np.ones(20)
+		tol2[14:] = 1.e5 * np.ones(6)
+		solver.atol = tol2
+		t, y, _ = solver.simulate(0.03)
+		plt.plot(t, np.fmod(y[:, 0:7], 2 * np.pi))
+		plt.ylim(-1, 1)
+		plt.show()
 
-	plt.legend()
-	plt.show()
+	def doIndex1():
+		problem = Seven_bar_mechanism_index1()
+		solver = IDA(problem)
+		# solver = Radau5DAE(problem)
+		tol1 = 1.e-6 * np.ones(20)
+		solver.atol = tol1
+		t, y, _ = solver.simulate(0.03)
+		plt.plot(t, np.fmod(y[:, 0:7], 2 * np.pi))
+		plt.ylim(-1, 1)
+		plt.show()
+
+	doIndex1()
